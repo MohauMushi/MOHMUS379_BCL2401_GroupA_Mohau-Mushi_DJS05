0e06e6ea43eaa65b16a714f4bdf31b8bcb3b6250
Mohau Mushi <david1mushi1@gmail.com>
2024-05-21 14:49:09 +0200

    refactor: Implemented action creators for counter, which will increment, decrement and reste to the initial state

--- actions.js
+++ actions.js
@@ -1,4 +1,26 @@
 import { states } from "./store.js";
 import { initialState } from "./initialState.js";
 
+export const addAction = () => {
+  return {
+    ...states[0],
+    value: states[0].value + 1,
+  };
+};
 
+export const subtractAction = () => {
+  return {
+    ...states[0],
+    value: states[0].value - 1,
+  };
+};
+
+export const resetAction = () => {
+  return {
+    ...initialState,
+  };
+};
+
+export const getState = (index) => {
+  console.log(states[0]);
+};
