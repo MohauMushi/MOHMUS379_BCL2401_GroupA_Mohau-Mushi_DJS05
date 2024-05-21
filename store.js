2d5ec3303d65d3811a9678bafeee74b1aa20a005
Mohau Mushi <david1mushi1@gmail.com>
2024-05-21 15:03:15 +0200

    Implemented state management with observers for dispatch, subscribe

--- store.js
+++ store.js
@@ -1,1 +1,26 @@
 import { initialState } from "./initialState.js";
+
+export const states = [initialState];
+  
+export const subscribers = [];
+
+export const dispatch = (action) => {
+	const prev = states[0];
+  
+	const next = action();
+  
+	subscribers.forEach((notify) => notify(prev, next));
+  
+	states.unshift(next);
+  };
+
+  export const subscribe = (notify) => {
+	subscribers.push(notify);
+  
+	const unsubscribe = () => {
+	  const result = subscribers.filter((current) => current !== notify);
+	  subscribers = result;
+	};
+  
+	return unsubscribe;
+  };
